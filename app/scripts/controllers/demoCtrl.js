88855db5b816b35ddbe19392bf9bacd6863c10eb
intuinno <intuinno@gmail.com>
2013-11-29 21:33:51 -0500

    Switched to Angular Version

---
+++ app/scripts/controllers/demoCtrl.js
@@ -0,0 +1,87 @@
+(function() {
+  'use strict';
+
+  angular.module('myApp.controllers')
+    .controller('DemoCtrl', ['$scope', 'd3Service',
+      function($scope, d3Service) {
+        $scope.greeting = "NomaRect";
+        //controller code here
+
+        $scope.d3Data = [{
+          name: "Greg",
+          score: 98
+        }, {
+          name: "Ari",
+          score: 96
+        }, {
+          name: 'Q',
+          score: 75
+        }, {
+          name: "Loser",
+          score: 48
+        }];
+
+        $scope.nomaConfig = {
+
+          
+
+        };
+
+        $scope.activeData = 'titanic';
+
+
+        d3Service.d3().then(function(d3) {
+          d3.tsv("data/Titanic.txt", function(error, tdata) {
+
+            $scope.nomaData = tdata;
+
+          });
+        });
+
+
+        $scope.onClick = function(item) {
+          $scope.$apply(function() {
+            if (!$scope.showDetailPanel)
+              $scope.showDetailPanel = true;
+            $scope.detailItem = item;
+          });
+        };
+
+
+
+        $scope.groups = [{
+          title: "Dynamic Group Header - 1",
+          content: "Dynamic Group Body - 1"
+        }, {
+          title: "Dynamic Group Header - 2",
+          content: "Dynamic Group Body - 2"
+        }];
+
+        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addItem = function() {
+          var newItemNo = $scope.items.length + 1;
+          $scope.items.push('Item ' + newItemNo);
+        };
+
+        $scope.changeActiveDataTitanic = function() {
+          $scope.activeData = 'Survivor of Titanic';
+
+          d3Service.d3().then(function(d3) {
+
+            d3.tsv("data/Titanic.txt", function(error, tdata) {
+
+              $scope.nomaData = tdata;
+
+            });
+          });
+
+        };
+
+
+      }
+    ]);
+
+
+
+}());
---
+++ app/scripts/directives/d3Basic.js
@@ -0,0 +1,337 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.directives')
+        .directive('nomarect', ['d3Service',
+            function(d3Service) {
+                return {
+                    restrict: 'EA',
+                    scope: {
+                        data: "=",
+                        onClick: '&',
+                        config: "="
+                    },
+
+                    link: function(scope, iElement, iAttrs) {
+
+                        d3Service.d3().then(function(d3) {
+
+                            var XMargin = 10;
+                            var YMargin = 2;
+
+                            var margin = parseInt(iAttrs.margin) || 20;
+                            var height = parseInt(iAttrs.height) || 400 - 2 * margin;
+                            var width = parseInt(iAttrs.width) || 1200 - 2 * margin;
+
+                            var color = d3.scale.category10();
+
+                            var x = d3.scale.linear()
+                                .range([0, width]);
+
+                            var y = d3.scale.linear()
+                                .range([height, 0]);
+
+                            var xAxis = d3.svg.axis()
+                                .scale(x)
+                                .orient("bottom");
+
+                            var yAxis = d3.svg.axis()
+                                .scale(y)
+                                .orient("left");
+
+                            var svg = d3.select(iElement[0])
+                                .append("svg")
+                                .attr("width", "100%")
+                                .append("g")
+                                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                            // on window resize, re-render d3 canvas
+                            window.onresize = function() {
+                                return scope.$apply();
+                            };
+
+                            scope.$watch(function() {
+                                return angular.element(window)[0].innerWidth;
+                            }, function() {
+                                return scope.render(scope.data);
+                            });
+
+                            // watch for data changes and re-render
+                            scope.$watch('data', function(newVals, oldVals) {
+                                return scope.render(newVals);
+                            }, true);
+
+
+
+                            //Gets the optimal width of rectangle based on 
+                            //Aspect ratio 
+                            var optimalNumElementWidthAspect = function(width, height, n) {
+
+                                var widthElement, heightElement;
+                                var numElementHeight;
+                                var optimalNumElementWidth;
+                                var optimalRatio = width * n / height;
+
+
+
+                                for (var numElementWidth = 1; numElementWidth < n + 1; numElementWidth++) {
+
+                                    widthElement = width / numElementWidth;
+                                    numElementHeight = Math.ceil(n / numElementWidth);
+                                    heightElement = height / numElementHeight;
+
+                                    var aspectRatio = widthElement / heightElement;
+
+                                    if (Math.abs(1 - aspectRatio) < Math.abs(1 - optimalRatio)) {
+
+                                        optimalNumElementWidth = numElementWidth;
+                                        optimalRatio = aspectRatio;
+                                    }
+
+                                }
+
+                                return optimalNumElementWidth;
+
+                            };
+
+                            //Gets the optimal height of rectangle based on 
+                            //Aspect ratio 
+                            var optimalNumElementHeightAspect = function(width, height, n) {
+
+                                var widthElement, heightElement;
+                                var numElementWidth;
+                                var optimalNumElementHeight;
+                                var optimalRatio = width * n / height;
+
+
+
+                                for (var numElementHeight = 1; numElementHeight < n + 1; numElementHeight++) {
+
+                                    heightElement = height / numElementHeight;
+                                    numElementWidth = Math.ceil(n / numElementHeight);
+                                    widthElement = width / numElementWidth;
+
+                                    var aspectRatio = widthElement / heightElement;
+
+                                    if (Math.abs(1 - aspectRatio) < Math.abs(1 - optimalRatio)) {
+
+                                        optimalNumElementHeight = numElementHeight;
+                                        optimalRatio = aspectRatio;
+                                    }
+
+                                }
+
+                                return optimalNumElementHeight;
+
+                            };
+
+
+
+                            var optimalNumElementWidthMargin = function(width, height, n) {
+
+                                var widthElement, heightElement;
+                                var numElementHeight;
+                                var optimalNumElementWidth;
+                                var optimalMargin = 1;
+                                var optimalRatio = width * n / height;
+
+
+                                for (numElementWidth = 1; numElementWidth < n + 1; numElementWidth++) {
+
+                                    widthElement = width / numElementWidth;
+                                    numElementHeight = Math.ceil(n / numElementWidth);
+                                    heightElement = height / numElementHeight;
+
+                                    var aspectRatio = widthElement / heightElement;
+
+                                    var margin = (n % numElementWidth) * widthElement * heightElement / width * height;
+
+                                    if (margin * Math.pow(Math.abs(1 - aspectRatio), 2) < optimalMargin * Math.pow(Math.abs(1 - optimalRatio), 2)) {
+
+                                        optimalNumElementWidth = numElementWidth;
+                                        optimalRatio = aspectRatio;
+                                        optimalMargin = margin;
+                                    }
+
+                                }
+
+                                return optimalNumElementWidth;
+
+                            };
+
+
+
+                            // define render function
+                            scope.render = function(data) {
+
+                                // remove all previous items before render
+                                //   svg.selectAll("*").remove();
+
+                                //   // setup variables
+                                //   var width, height, max;
+                                //   width = d3.select(iElement[0]).node().offsetWidth - margin;
+                                //   // 20 is for margins and can be changed
+                                //   height = scope.data.length * (barHeight + barPadding);
+
+
+                                if (!data) return;
+
+                                var initialSquareLenth = 10;
+
+                                xAxis.orient("bottom");
+
+                                yAxis.orient("left");
+
+                                d3.select(".x").call(xAxis);
+                                d3.select(".y").call(yAxis);
+
+                                var selection_order = ['Yes', 'No'];
+                                var class_order = ['First', 'Second', 'Third', 'Crew'];
+                                var gender_order = ['Male', 'Female'];
+
+                                var nest = d3.nest()
+                                    .key(function(d) {
+                                        return d.Class;
+                                    })
+                                    .sortKeys(function(a, b) {
+                                        return class_order.indexOf(a) - class_order.indexOf(b);
+                                    })
+                                    .key(function(d) {
+                                        return d.Sex;
+                                    })
+                                    .sortKeys(function(a, b) {
+                                        return gender_order.indexOf(a) - gender_order.indexOf(b);
+                                    })
+                                    .sortValues(function(a, b) {
+                                        return selection_order.indexOf(a.Survived) - selection_order.indexOf(b.Survived);
+                                    })
+                                    .entries(data);
+
+                                var sum = nest.reduce(function(previousValue, currentParent) {
+                                    return (currentParent.offset = previousValue) + (currentParent.sum = currentParent.values.reduceRight(function(previousValue, currentChild) {
+                                        currentChild.parent = currentParent;
+                                        return (currentChild.offset = previousValue) + currentChild.values.length;
+                                    }, 0));
+                                }, 0);
+
+                                var XOffset = 0;
+                                var YOffset = 0;
+
+                                var XnumGroup = nest.length;
+                                var YnumGroup = nest[0].values.length;
+
+                                nest.forEach(function(d, i, j) {
+
+                                    //Here d is PassengerClass Array
+
+                                    var count = 0;
+                                    var tempXWidth = 0;
+                                    var tempYHeight = 0;
+
+                                    YOffset = 0;
+
+
+                                    d.values.forEach(function(d, i, j) {
+
+                                        //Here d is Gender Array
+                                        tempXWidth = 0;
+                                        count = 0;
+
+                                        d.values.forEach(function(d, i, j) {
+
+                                            //Here d is object
+                                            d.tempID = count;
+                                            count += 1;
+
+                                        });
+
+                                        tempXWidth = optimalNumElementWidthAspect(width / XnumGroup, height / YnumGroup, count);
+
+                                        tempYHeight = Math.ceil(count / tempXWidth);
+
+                                        d.values.forEach(function(d, i, j) {
+
+                                            d.tempXGroupSize = count;
+                                            d.tempXWidth = tempXWidth;
+                                            d.tempXOffset = XOffset;
+                                            d.tempYOffset = YOffset;
+                                            d.tempYHeight = tempYHeight;
+                                            d.widthRatio = Math.sqrt(sum) / XnumGroup / tempXWidth;
+                                            d.heightRatio = Math.sqrt(sum) / YnumGroup / tempYHeight;
+
+                                        });
+
+
+                                        YOffset += Math.sqrt(sum) / YnumGroup * 10 + 10;
+
+
+                                    });
+
+
+                                    XOffset += Math.sqrt(sum) / XnumGroup * 10 + 10;
+
+                                });
+
+                                svg.append("g")
+                                    .attr("class", "x axis")
+                                    .attr("transform", "translate(0," + height + ")")
+                                    .call(xAxis)
+                                    .append("text")
+                                    .attr("class", "label")
+                                    .attr("x", width)
+                                    .attr("y", -6)
+                                    .style("text-anchor", "end")
+                                    .text("Sepal Width (cm)");
+
+                                svg.append("g")
+                                    .attr("class", "y axis")
+                                    .call(yAxis)
+                                    .append("text")
+                                    .attr("class", "label")
+                                    .attr("transform", "rotate(-90)")
+                                    .attr("y", 6)
+                                    .attr("dy", ".71em")
+                                    .style("text-anchor", "end")
+                                    .text("Sepal Length (cm)")
+
+                                svg.selectAll(".dot")
+                                    .data(data)
+                                    .enter().append("rect")
+                                    .attr("class", "dot")
+                                    .attr("width", function(d) {
+                                        return 10 * d.widthRatio;
+                                    })
+                                    .attr("height", function(d) {
+                                        return 10 * d.heightRatio;
+                                    })
+                                    .attr("rx", 0)
+                                    .attr("ry", 0)
+                                    .transition()
+                                    .duration(1000)
+                                    .attr("x", function(d) {
+                                        return (+d.tempID % (+d.tempXWidth)) * 10 * d.widthRatio;
+                                    })
+                                    .attr("y", function(d) {
+                                        return height - (Math.floor(+d.tempID / (+d.tempXWidth)) + 1) * 10 * d.heightRatio;
+                                    })
+                                    .style("fill", function(d) {
+                                        return color(d.Survived);
+                                    })
+                                    .attr("transform", function(d, i) {
+                                        return "translate(" + (+d.tempXOffset) + "," + (-d.tempYOffset) + ")";
+                                    });
+
+
+                            }; //End renderer
+
+
+                        }); //End Service
+                    }
+
+                } //End return 
+
+            } // End function (d3Service)
+
+        ]);
+
+}());
