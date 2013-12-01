c7df97be4caeffcd1b949e94ed64cfcbb2355052
intuinno <intuinno@gmail.com>
2013-12-01 17:54:48 -0500

    mammo Data change working
    [Done]
     - Load mammo
    [ToDo]
     - Load interface

--- app/scripts/controllers/demoCtrl.js
+++ app/scripts/controllers/demoCtrl.js
@@ -4,60 +4,15 @@
     angular.module('myApp.controllers')
         .controller('DemoCtrl', ['$scope', 'd3Service',
             function($scope, d3Service) {
-                $scope.greeting = "NomaRect";
-                //controller code here
 
                 $scope.nomaConfig = {
 
-
+                  //initial Data
+                  
 
                 };
-                $scope.nomaConfig.dims = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
-                $scope.nomaConfig.xDim = $scope.nomaConfig.dims[0];
-                $scope.nomaConfig.yDim = $scope.nomaConfig.dims[1];
-                $scope.nomaConfig.colorDim = $scope.nomaConfig.dims[2];
 
-                $scope.d3Data = [{
-                    name: "Greg",
-                    score: 98
-                }, {
-                    name: "Ari",
-                    score: 96
-                }, {
-                    name: 'Q',
-                    score: 75
-                }, {
-                    name: "Loser",
-                    score: 48
-                }];
-
-
-
-                $scope.activeData = 'titanic';
-
-
-                d3Service.d3().then(function(d3) {
-                    d3.tsv("data/Titanic.txt", function(error, tdata) {
-                        var count = 0;
-
-                        tdata.map(function(d) {
-                            d.id = count;
-                            count += 1;
-                        })
-
-                        $scope.nomaData = tdata;
-                        $scope.nomaConfig.dims = d3.keys(tdata[0]);
-                        $scope.nomaConfig.xDim = $scope.nomaConfig.dims[0];
-                        $scope.nomaConfig.yDim = $scope.nomaConfig.dims[1];
-                        $scope.nomaConfig.colorDim = $scope.nomaConfig.dims[2];
-
-
-
-                        $scope.$apply();
-
-                    });
-                });
-
+                $scope.loadedData = 'titanic';
 
                 $scope.onClick = function(item) {
                     $scope.$apply(function() {
@@ -67,7 +22,40 @@
                     });
                 };
 
+                $scope.loadData = function() {
 
+
+                   switch ($scope.activeData) {
+
+                        case 'titanic':
+                            changeActiveDataTitanic();
+                            break;
+                        case 'mammo':
+                            changeActiveDataMammo();
+                            break;
+
+                          }
+
+
+
+                };
+
+
+                // $scope.setData = function() {
+
+                //     switch ($scope.activeData) {
+
+                //         case 'Survivor of Titanic':
+                //             $scope.nomaConfig.data.isTitanic = true;
+                //             $scope.nomaConfig.data.isMammo = false;
+                //             break;
+                //         case 'Bayesian Inference - Mammogram':
+                //             $scope.nomaConfig.data.isTitanic = false;
+                //             $scope.nomaConfig.data.isMammo = true;
+                //             break;
+
+                //     }
+                // };
 
                 $scope.groups = [{
                     title: "Dynamic Group Header - 1",
@@ -85,20 +73,134 @@
                 };
 
                 $scope.changeActiveDataTitanic = function() {
+
+
                     $scope.activeData = 'Survivor of Titanic';
+                    // $scope.setData();
+                    
+
+
+
 
                     d3Service.d3().then(function(d3) {
-
                         d3.tsv("data/Titanic.txt", function(error, tdata) {
+                            var count = 0;
+
+                            tdata.map(function(d) {
+                                d.id = count;
+                                count += 1;
+                            })
 
                             $scope.nomaData = tdata;
+                            $scope.nomaConfig.dims = d3.keys(tdata[0]);
+
+                            var index = $scope.nomaConfig.dims.indexOf("id");
+                            $scope.nomaConfig.dims.splice(index, 1);
+
+                            $scope.nomaConfig.xDim = $scope.nomaConfig.dims[0];
+                            $scope.nomaConfig.yDim = $scope.nomaConfig.dims[1];
+                            $scope.nomaConfig.colorDim = $scope.nomaConfig.dims[2];
+
+
+                            $scope.$apply();
+
+
 
                         });
                     });
 
 
 
-                };
+                }; //End  $scope.changeActiveDataTitanic()
+
+                $scope.changeActiveDataTitanic();
+
+
+
+
+                ///////////////////////////////////////////////////////////
+                ///////////////////////////////////////////////////////////
+                ///////////////////////////////////////////////////////////
+                // change Active Data to the Bayesian Inference-Mammogram;
+
+                $scope.changeActiveDataMammo = function() {
+
+                    //Config settings
+                    var numberOfEntity = 1000;
+                    var numDiscreteVar = 60;
+
+                    $scope.activeData = 'Bayesian Inference - Mammogram';
+                    // $scope.nomaConfig.data.isMammo = true;
+                    //  $scope.setData();
+
+                    var data = [];
+
+                    for (var count = 0; count < numberOfEntity; count++) {
+
+                        var temp = new Object();
+
+                        temp.id = count;
+
+                        // temp.continous_variable1 = Math.random();
+                        //temp.continous_variable2 = Math.random();
+                        temp.discrete_variable = Math.round(Math.random() * (numDiscreteVar - 1));
+
+                        if (Math.random() > 0.3) {
+                            temp.nominal_variable = 'Male';
+                        } else {
+                            temp.nominal_variable = 'Female';
+                        }
+
+                        if (Math.random() > 0.99) {
+                            temp.cancer = 'Cancer';
+
+                            if (Math.random() > 0.8) {
+                                temp.mamo = 'Negative Mamo';
+                            } else {
+                                temp.mamo = 'Positive Mamo';
+                            }
+
+                        } else {
+                            temp.cancer = 'No Cancer';
+
+                            if (Math.random() > 0.096) {
+                                temp.mamo = 'Negative Mamo';
+                            } else {
+                                temp.mamo = 'Positive Mamo';
+                            }
+                        }
+
+                        temp.descriptor = temp.cancer + ", " + temp.mamo;
+
+                        // if (temp.continous_variable1 * temp.continous_variable2 > 0.7) {
+                        //     temp.selection_variable = 'Group 1';
+
+                        // } else if (temp.continous_variable1 * temp.continous_variable2 > 0.5) {
+                        //     temp.selection_variable = 'Group 1 & 2';
+                        // } else if (temp.continous_variable1 * temp.continous_variable2 > 0.3) {
+                        //     temp.selection_variable = 'Group 2';
+                        // } else {
+                        //     temp.selection_variable = 'None';
+                        // }
+
+
+                        data.push(temp);
+                    }
+
+                    $scope.nomaData = data;
+                    $scope.nomaConfig.dims = d3.keys(data[0]);
+
+                    var index = $scope.nomaConfig.dims.indexOf("id");
+                    $scope.nomaConfig.dims.splice(index, 1);
+
+                    $scope.nomaConfig.xDim = $scope.nomaConfig.dims[0];
+                    $scope.nomaConfig.yDim = $scope.nomaConfig.dims[1];
+                    $scope.nomaConfig.colorDim = $scope.nomaConfig.dims[2];
+
+                    // $scope.$apply();
+
+
+                }; //End  $scope.changeActiveDataMammo()
 
 
             }
--- app/scripts/directives/d3Basic.js
+++ app/scripts/directives/d3Basic.js
@@ -78,7 +78,7 @@
 
                                 var widthElement, heightElement;
                                 var numElementHeight;
-                                var optimalNumElementWidth;
+                                var optimalNumElementWidth = 1;
                                 var optimalRatio = width * n / height;
 
 
@@ -109,7 +109,7 @@
 
                                 var widthElement, heightElement;
                                 var numElementWidth;
-                                var optimalNumElementHeight;
+                                var optimalNumElementHeight = 1;
                                 var optimalRatio = width * n / height;
 
 
@@ -140,7 +140,7 @@
 
                                 var widthElement, heightElement;
                                 var numElementHeight;
-                                var optimalNumElementWidth;
+                                var optimalNumElementWidth  =1;
                                 var optimalMargin = 1;
                                 var optimalRatio = width * n / height;
 
@@ -183,203 +183,16 @@
 
                                 if (!data) return;
 
+                                svgGroup.selectAll("*").remove();
 
-                                var widthSVG = d3.select(iElement[0]).node().offsetWidth;
-                                // calculate the height
-                                var heightSVG = d3.select(iElement[0]).node().offsetWidth / 2;
-
-                                svg.attr('height', heightSVG);
-                                svg.attr('width', widthSVG);
-
-                                var x = d3.scale.linear()
-                                    .range([0, width]);
-
-                                var y = d3.scale.linear()
-                                    .range([height, 0]);
-
-                                var xAxis = d3.svg.axis()
-                                    .scale(x)
-                                    .orient("bottom");
-
-                                var yAxis = d3.svg.axis()
-                                    .scale(y)
-                                    .orient("left");
-
-                               
-
-
-                                xAxis.orient("bottom");
-
-                                yAxis.orient("left");
-
-                                d3.select(".x").call(xAxis);
-                                d3.select(".y").call(yAxis);
-
-                                var selection_order = ['Yes', 'No'];
-                                var class_order = ['First', 'Second', 'Third', 'Crew'];
-                                var initialSquareLenth = 10;
-                                var gender_order = ['Male', 'Female'];
-
-                                var nest = d3.nest()
-                                    .key(function(d) {
-                                        return d.Class;
-                                    })
-                                    .sortKeys(function(a, b) {
-                                        return class_order.indexOf(a) - class_order.indexOf(b);
-                                    })
-                                    .key(function(d) {
-                                        return d.Sex;
-                                    })
-                                    .sortKeys(function(a, b) {
-                                        return gender_order.indexOf(a) - gender_order.indexOf(b);
-                                    })
-                                    .sortValues(function(a, b) {
-                                        return selection_order.indexOf(a.Survived) - selection_order.indexOf(b.Survived);
-                                    })
-                                    .entries(data);
-
-                                var sum = nest.reduce(function(previousValue, currentParent) {
-                                    return (currentParent.offset = previousValue) + (currentParent.sum = currentParent.values.reduceRight(function(previousValue, currentChild) {
-                                        currentChild.parent = currentParent;
-                                        return (currentChild.offset = previousValue) + currentChild.values.length;
-                                    }, 0));
-                                }, 0);
-
-                                var XOffset = 0;
-                                var YOffset = 0;
-
-                                var XnumGroup = nest.length;
-                                var YnumGroup = nest[0].values.length;
-
-                                nest.forEach(function(d, i, j) {
-
-                                    //Here d is PassengerClass Array
-
-                                    var count = 0;
-                                    var tempXWidth = 0;
-                                    var tempYHeight = 0;
-
-                                    YOffset = 0;
-
-
-                                    d.values.forEach(function(d, i, j) {
-
-                                        //Here d is Gender Array
-                                        tempXWidth = 0;
-                                        count = 0;
-
-                                        d.values.forEach(function(d, i, j) {
-
-                                            //Here d is object
-                                            d.tempID = count;
-                                            count += 1;
-
-                                        });
-
-                                        tempXWidth = optimalNumElementWidthAspect(width / XnumGroup, height / YnumGroup, count);
-
-                                        tempYHeight = Math.ceil(count / tempXWidth);
-
-                                        d.values.forEach(function(d, i, j) {
-
-                                            d.tempXGroupSize = count;
-                                            d.tempXWidth = tempXWidth;
-                                            d.tempXOffset = XOffset;
-                                            d.tempYOffset = YOffset;
-                                            d.tempYHeight = tempYHeight;
-                                            d.widthRatio = Math.sqrt(sum) / XnumGroup / tempXWidth;
-                                            d.heightRatio = Math.sqrt(sum) / YnumGroup / tempYHeight;
-
-                                        });
-
-
-                                        YOffset += Math.sqrt(sum) / YnumGroup * initialSquareLenth + 10;
-
-
-                                    });
-
-
-               
-
-                             XOffset += Math.sqrt(sum) / XnumGroup * initialSquareLenth + 10;
-
-                                });
-
-                                x = d3.scale.ordinal()
-                                    .rangeRoundBands([0, width], 1)
-                                    .domain(nest.map(function(d) {
-                                        return d.key;
-                                    }));
-
-                                y = d3.scale.ordinal()
-                                    .rangeRoundBands([height, 0], 1)
-                                    .domain(nest[0].values.map(function(d) {
-                                        return d.key;
-                                    }));
-
-                                xAxis = d3.svg.axis()
-                                    .scale(x)
-                                    .orient("bottom");
-
-                                yAxis = d3.svg.axis()
-                                    .scale(y)
-                                    .orient("left");
-
-                                svg.selectAll(".axis").remove();
-
-                                svgGroup.append("g")
-                                    .attr("class", "x axis")
-                                    .attr("transform", "translate(0," + height + ")")
-                                    .call(xAxis)
-                                    .append("text")
-                                    .attr("class", "axislabel")
-                                    .attr("x", width / 2)
-                                    .attr("y", 56)
-                                    .style("text-anchor", "end")
-                                    .text("Passenger Class");
-
-                                svgGroup.append("g")
-                                    .attr("class", "y axis")
-                                    .call(yAxis)
-                                    .append("text")
-                                    .attr("class", "axislabel")
-                                    .attr("transform", "rotate(-90)")
-                                    .attr("x", -height / 2)
-                                    .attr("y", -50)
-                                    .attr("dy", ".71em")
-                                    .style("text-anchor", "end")
-                                    .text("Gender")
 
                                 svgGroup.selectAll(".dot")
                                     .data(data)
                                     .enter().append("rect")
-                                    .attr("class", "dot")
-                                    .attr("width", function(d) {
-                                        // console.log(initialSquareLenth);
-                                        return initialSquareLenth * d.widthRatio;
-                                    })
-                                    .attr("height", function(d) {
-                                        return initialSquareLenth * d.heightRatio;
-                                    })
-                                    .attr("rx", 0)
-                                    .attr("ry", 0)
-                                    .transition()
-                                    .duration(1000)
-                                    .attr("x", function(d) {
-                                        return (+d.tempID % (+d.tempXWidth)) * initialSquareLenth * d.widthRatio;
-                                    })
-                                    .attr("y", function(d) {
-                                        return height - (Math.floor(+d.tempID / (+d.tempXWidth)) + 1) * initialSquareLenth * d.heightRatio;
-                                    })
-                                    .style("fill", function(d) {
-                                        return color(d.Survived);
-                                    })
-                                    .attr("transform", function(d, i) {
-                                        return "translate(" + (+d.tempXOffset) + "," + (-d.tempYOffset) + ")";
-                                    });
+                                    .attr("class", "dot");
 
 
-                            }; //End renderer
+                            }; //End Data change renderer
 
 
 
@@ -440,9 +253,6 @@
                                     return d.key;
                                 });
 
-                               
-
-
 
                                 nest = d3.nest()
                                     .key(function(d) {
@@ -472,8 +282,8 @@
                                 var XOffset = 0;
                                 var YOffset = 0;
 
-                                var XnumGroup = nest.length;
-                                var YnumGroup = nest[0].values.length;
+                                var XnumGroup = config.xDimOrder.length;
+                                var YnumGroup = config.yDimOrder.length;
 
                                 nest.forEach(function(d, i, j) {
 
@@ -529,15 +339,12 @@
 
 
 
-
-
-
                                 var x = d3.scale.ordinal()
-                                    .rangeRoundBands([0, width], 1)
+                                    .rangeRoundBands([0, width], 0.2,0.1)
                                     .domain(config.xDimOrder);
 
                                 var y = d3.scale.ordinal()
-                                    .rangeRoundBands([height, 0], 1)
+                                    .rangeRoundBands([height, 0], 0.2,0.1)
                                     .domain(config.yDimOrder);
 
                                 var xAxis = d3.svg.axis()
@@ -589,16 +396,16 @@
                                     .transition()
                                     .duration(1000)
                                     .attr("x", function(d) {
-                                        return (+d.tempID % (+d.tempXWidth)) * initialSquareLenth * d.widthRatio;
+                                        return width/(XnumGroup+1)/4 + (+d.tempID % (+d.tempXWidth)) * initialSquareLenth * d.widthRatio;
                                     })
                                     .attr("y", function(d) {
-                                        return height - (Math.floor(+d.tempID / (+d.tempXWidth)) + 1) * initialSquareLenth * d.heightRatio;
+                                        return  height/(YnumGroup+1) - (Math.floor(+d.tempID / (+d.tempXWidth)) + 1) * initialSquareLenth * d.heightRatio;
                                     })
                                     .style("fill", function(d) {
                                         return color(d[config.colorDim]);
                                     })
                                     .attr("transform", function(d, i) {
-                                        return "translate(" + (+d.tempXOffset) + "," + (-d.tempYOffset) + ")";
+                                        return "translate(" + x(d[config.xDim]) + "," + (y(d[config.yDim])) + ")";
                                     });
 
                                 var legendGroup = svg.selectAll(".legend")
