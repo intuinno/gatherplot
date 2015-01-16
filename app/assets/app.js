9f3c416155c62a13af0a2e6e4d08475b54f324be
intuinno <intuinno@gmail.com>
2015-01-16 02:09:12 -0500

    Introduced ngRoute and refactored index.html
    Added partial templates

--- app/assets/app.js
+++ app/assets/app.js
@@ -64065,12 +64065,28 @@ angular.module('ui.sortable', [])
         'myApp.controllers',
         'myApp.directives',
         'ngRoute'
-    ]);
+    ]).config(['$routeProvider', 
+        function($routeProvider) {
+            $routeProvider.
+                when('/demo', {
+                    templateUrl: '../templates/partials/index_full.html',
+                    controllers: 'DemoCtrl'
+                }).
+                when('/show/:dataset/:xDim/:yDim/:colorDim/:relativeMode', {
+                    templateUrl: '../templates/partials/index_simple.html',
+                    controllers: 'showCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/demo'
+                });
+
+        }]);;
 
     // setup dependency injection
     angular.module('myApp.controllers', []);
     angular.module('myApp.directives', [ 'ui.bootstrap', 'ui.sortable']);
 
+    
 
 }());;(function() {
     'use strict';
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,11 +6,27 @@
         'myApp.controllers',
         'myApp.directives',
         'ngRoute'
-    ]);
+    ]).config(['$routeProvider', 
+        function($routeProvider) {
+            $routeProvider.
+                when('/demo', {
+                    templateUrl: '../templates/partials/index_full.html',
+                    controllers: 'DemoCtrl'
+                }).
+                when('/show/:dataset/:xDim/:yDim/:colorDim/:relativeMode', {
+                    templateUrl: '../templates/partials/index_simple.html',
+                    controllers: 'showCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/demo'
+                });
+
+        }]);;
 
     // setup dependency injection
     angular.module('myApp.controllers', []);
     angular.module('myApp.directives', [ 'ui.bootstrap', 'ui.sortable']);
 
+    
 
 }());
